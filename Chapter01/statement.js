3bb22ea686e0d60faa82fbf3a59fe35d910ce241
codeblue25 <chois1031@gmail.com>
2023-10-10 23:22:05 +0900

    챕터1: 함수 추출하기

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,3 +1,30 @@
+// 한 번의 공연에 대한 요금을 계산하는 함수
+function amountFor(perf, play) {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount;
+}
+
 // 공연료 청구서를 출력하는 함수
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -13,25 +40,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play)
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
