6c514523ae4c6a52cf8dbd52721174d03ca2be01
codeblue25 <chois1031@gmail.com>
2023-10-10 23:25:14 +0900

    챕터1: 변수, 인수 이름 바꾸기

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -1,28 +1,28 @@
 // 한 번의 공연에 대한 요금을 계산하는 함수
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function amountFor(aPerformance, play) {
+  let result = 0;
 
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
 
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
 
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
 
-  return thisAmount;
+  return result;
 }
 
 // 공연료 청구서를 출력하는 함수
