f7bedd86b8bbcef945830ddfb25cdb5f63c3d819
codeblue25 <chois1031@gmail.com>
2023-10-18 23:35:53 +0900

    챕터1: 중간 데이터 사용하도록 변경

--- Chapter01/statement.js
+++ Chapter01/statement.js
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -61,7 +61,7 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
     volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) {
+    if ("comedy" === aPerformance.play.type) {
       volumeCredits += Math.floor(aPerformance.audience / 5);
     }
     return volumeCredits;
@@ -71,7 +71,7 @@ function renderPlainText(data, plays) {
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -88,7 +88,7 @@ function renderPlainText(data, plays) {
         break;
 
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
     
     return result;
